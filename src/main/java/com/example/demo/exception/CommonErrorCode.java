3672b0768a9f126c5b3a1066941991af525dea85
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-06-02 18:49:42 +0900

    feat: CommonErrorCode 구현

---
+++ src/main/java/com/example/demo/exception/CommonErrorCode.java
@@ -0,0 +1,28 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum CommonErrorCode implements ErrorCode{
+
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "자원이 존재하지 않습니다."),
+    NULL_PARAMETER(HttpStatus.BAD_REQUEST, "NULL 파라미터가 존재합니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    CommonErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+}
