0f8680ad886df3e7e14b5adedb9e44c74c64a4f9
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-06-02 18:49:36 +0900

    feat: MemberErrorCode 구현

---
+++ src/main/java/com/example/demo/exception/MemberErrorCode.java
@@ -0,0 +1,28 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum MemberErrorCode implements ErrorCode{
+
+    EMAIL_CONFLICT(HttpStatus.CONFLICT, "중복된 이메일입니다."),
+    ARTICLE_EXISTENCE(HttpStatus.BAD_REQUEST, "작성한 게시물이 존재합니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    MemberErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+}
