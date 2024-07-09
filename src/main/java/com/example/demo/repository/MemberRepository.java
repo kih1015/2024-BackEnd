4c304655d8baebf91b2443be4a9b0dbdbef45838
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-07-09 23:24:33 +0900

    feat: 이메일로 검색 기능 추가

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -4,12 +4,15 @@ import com.example.demo.domain.Member;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends Repository<Member, Long> {
 
     List<Member> findAll();
 
     Member findById(Long id);
+
+    Optional<Member> findByEmail(String email);
 
     Member save(Member member);
 
