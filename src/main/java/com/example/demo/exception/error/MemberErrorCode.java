82c04a81501b9931e27364d382c13201c184723d
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-07-09 23:25:38 +0900

    feat: 이메일 존재하지 않음, 패스워드 불일치 에러 추가

--- src/main/java/com/example/demo/exception/error/MemberErrorCode.java
+++ src/main/java/com/example/demo/exception/error/MemberErrorCode.java
@@ -4,8 +4,10 @@ import org.springframework.http.HttpStatus;
 
 public enum MemberErrorCode implements ErrorCode{
 
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "이메일을 찾을 수 없습니다."),
     EMAIL_CONFLICT(HttpStatus.CONFLICT, "중복된 이메일입니다."),
-    ARTICLE_EXISTENCE(HttpStatus.BAD_REQUEST, "작성한 게시물이 존재합니다.");
+    ARTICLE_EXISTENCE(HttpStatus.BAD_REQUEST, "작성한 게시물이 존재합니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "패스워드를 잘못 입력했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
