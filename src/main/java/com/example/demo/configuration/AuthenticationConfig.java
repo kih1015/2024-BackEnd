21f7c37b7acbe10989d8d08072552464d35e07a3
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-07-09 21:31:15 +0900

    feat: add AuthenticationConfig

---
+++ src/main/java/com/example/demo/configuration/AuthenticationConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AuthenticationConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/members/join").permitAll()
+                        .requestMatchers("/members/login").permitAll()
+                        .requestMatchers(HttpMethod.POST).authenticated()
+                        .anyRequest().authenticated());
+        return httpSecurity.build();
+    }
+
+}
